34f4982a5d75b46d5869af249bf52e1f897fb244
Łukasz Biel <lukasz.biel@10g.pl>
2019-04-07 13:07:04 +0200

    Sec commit

--- src/main/java/com/github/travelervihaan/sqltranslator/controller/TranslatorController.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/controller/TranslatorController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TranslatorController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String home(){
+        return "index";
+    }
+
+    @RequestMapping("/translate")
+    public String translate(){
         return "index";
     }
 }
