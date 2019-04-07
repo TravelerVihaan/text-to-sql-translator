172001625d3f9c2562da9b47463453e0f58639a5
Łukasz Biel <lukasz.biel@10g.pl>
2019-04-07 12:47:28 +0200

    Sec commit

--- src/main/java/com/github/travelervihaan/sqltranslator/model/Dictionary.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/model/Dictionary.java
@@ -2,6 +2,11 @@ package com.github.travelervihaan.sqltranslator.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class Dictionary {
+
+    private List<String> select;
+
 }
