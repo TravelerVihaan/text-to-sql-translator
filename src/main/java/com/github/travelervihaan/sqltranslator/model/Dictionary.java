75c60bcaae7acf1bee54df4c766c1efad2e3d714
Łukasz Biel <lukasz.biel@10g.pl>
2019-04-07 12:26:06 +0200

    Sec commit

--- src/main/java/com/github/travelervihaan/sqltranslator/model/Dictionary.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/model/Dictionary.java
@@ -1,4 +1,7 @@
 package com.github.travelervihaan.sqltranslator.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dictionary {
 }
--- src/main/java/com/github/travelervihaan/sqltranslator/service/DictionaryService.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/service/DictionaryService.java
@@ -1,4 +1,7 @@
 package com.github.travelervihaan.sqltranslator.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DictionaryService {
 }
