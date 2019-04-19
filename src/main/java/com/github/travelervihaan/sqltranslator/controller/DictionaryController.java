d0b749d9bcf8cb90024875cc99d80866f5223958
TravelerVihaan <lukasz.biel@10g.pl>
2019-04-19 14:03:03 +0200

    Added DictionaryController

---
+++ src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
@@ -0,0 +1,9 @@
+package com.github.travelervihaan.sqltranslator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dictionary")
+public class DictionaryController {
+}
