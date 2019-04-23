67832b0f58d4de4bbf15cba0020d9fede26364a6
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2019-04-23 16:25:35 +0200

    Added dictionary page

--- src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
@@ -1,9 +1,27 @@
 package com.github.travelervihaan.sqltranslator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.travelervihaan.sqltranslator.service.DictionaryService;
 
 @Controller
 @RequestMapping("/dictionary")
 public class DictionaryController {
+	
+	private DictionaryService dictionaryService;
+	
+	@Autowired
+	public DictionaryController(DictionaryService dictionaryService) {
+		this.dictionaryService = dictionaryService;
+	}
+	
+	@GetMapping("/")
+	public String dictionaries(Model model) {
+		model.addAttribute("dictionaries", dictionaryService.getAllDictionaries());
+		return "dictionary/dictionary";
+	}
 }
