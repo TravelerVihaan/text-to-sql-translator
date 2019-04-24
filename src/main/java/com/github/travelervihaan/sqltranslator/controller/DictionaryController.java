7d51edac535860ea2cf7f8afddcec5560fa267d9
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2019-04-24 13:17:48 +0200

    Modified dictionary page frontend

--- src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.github.travelervihaan.sqltranslator.service.DictionaryService;
@@ -23,5 +24,10 @@ public class DictionaryController {
 	public String dictionaries(Model model) {
 		model.addAttribute("dictionaries", dictionaryService.getAllDictionaries());
 		return "dictionary/dictionary";
+	}
+	
+	@PostMapping(value= {"/",""})
+	public String modyfiyDictionary() {
+		return "redirect:/dictionary";
 	}
 }
