0973ffc1561077794ce8580192e14b6e522049a1
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2019-04-26 10:34:45 +0200

    Add files via upload

--- src/main/java/com/github/travelervihaan/sqltranslator/controller/TranslatorController.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/controller/TranslatorController.java
@@ -1,10 +1,22 @@
 package com.github.travelervihaan.sqltranslator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.travelervihaan.sqltranslator.service.TranslatorService;
 
 @Controller
 public class TranslatorController {
+	
+	private TranslatorService translatorService;
+	
+	@Autowired
+	public TranslatorController(TranslatorService translatorService) {
+		this.translatorService = translatorService;
+	}
 
     @RequestMapping("/")
     public String home(){
@@ -12,7 +24,8 @@ public class TranslatorController {
     }
 
     @RequestMapping("/translate")
-    public String translate(){
+    public String translate(@RequestParam String statement, Model model){
+    	translatorService.setNaturalLanguageStatement(statement);
         return "redirect:/index";
     }
 }
