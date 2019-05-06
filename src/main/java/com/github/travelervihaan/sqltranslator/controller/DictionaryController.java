f8ae14d16fba883e639d13e92cc2a6f868f8d22e
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2019-05-06 16:06:08 +0200

    Add files via upload

--- src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
+++ src/main/java/com/github/travelervihaan/sqltranslator/controller/DictionaryController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,5 +40,10 @@ public class DictionaryController {
 		model.addAttribute("dictionary",dictionaryService.getByName(dictName));
 		return "dictionary/specifieddictionary";
 	}
-
+	
+	@PostMapping("/dictionarypanel")
+	public String specifiedDictionaryPost(Model model) {
+		return "redirect:/specifieddictionary";
+	}
+		
 }
